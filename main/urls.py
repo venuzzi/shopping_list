7ae0d102821f3acf2a7b3e4df7247d656df53b8a
venuzzi <rifqipratama2004@gmail.com>
2023-09-20 15:40:58 +0700

    commit tutor 3

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from main.views import show_main
 from django.urls import path, include
 from main.views import show_main, create_product, show_xml, show_json
-
+from main.views import register, login_user, logout_user#sesuaikan dengan nama fungsi yang dibuat
 
 app_name = 'main'
 
@@ -11,4 +11,9 @@ urlpatterns = [
     path('create-product', create_product, name='create_product'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'), 
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
+    
+
 ]
